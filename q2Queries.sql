b760b0af7ac6a5b07eab2fb39fbcc4fcff5ce42b
papapetrou <odysseasp@gmail.com>
2019-02-18 12:45:44 +0100

    Create q2Queries.sql

---
+++ q2Queries.sql
@@ -0,0 +1,13 @@
+-- ATTENTION: Remove the comments and the example before submitting
+-- This file should contain exactly 8 lines, without any comment. Each line will correspond to one of the 8 sub-questions of question 2.
+-- Do not break long queries to more than one line, and do not leave empty lines between them. 
+-- If you do not know the answer to one of the sub-questions, then write SELECT 0;
+-- Example: Let's say I only know sub-question 3:
+SELECT 0;
+SELECT 0;
+SELECT phonenumber from myphonebook where name=%1%; 
+SELECT 0;
+SELECT 0;
+SELECT 0;
+SELECT 0;
+SELECT 0;
