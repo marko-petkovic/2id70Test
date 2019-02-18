6fb7273e10902568bcf2ec259bacc929fc2243c0
papapetrou <odysseasp@gmail.com>
2019-02-18 12:39:49 +0100

    Create q2Create.sql

---
+++ q2Create.sql
@@ -0,0 +1,7 @@
+-- this file should contain the code required to create the auxiliary structures, for query 2
+-- this file will be given 10 minutes to run. Then it will be killed
+-- The file will be invoked with timeout 10m psql -d uni -f q2Create.sql
+-- Remember that the database (including the auxiliary structures) needs to be less than 11 GB.
+
+CREATE INDEX IDX_PHONEBOOK  
+CREATE INDEX IDX_ASR ON ActivestudentRegistrations (DegreeID); -- 16 seconds
