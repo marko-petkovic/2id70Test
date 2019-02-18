4c84113faf5dd4182b454d145bd9dd3d84353d08
papapetrou <odysseasp@gmail.com>
2019-02-18 12:41:22 +0100

    Update q2Create.sql

--- q2Create.sql
+++ q2Create.sql
@@ -2,6 +2,6 @@
 -- this file will be given 10 minutes to run. Then it will be killed
 -- The file will be invoked with timeout 10m psql -d uni -f q2Create.sql
 -- Remember that the database (including the auxiliary structures) needs to be less than 11 GB.
-
-CREATE INDEX IDX_PHONEBOOK  
-CREATE INDEX IDX_ASR ON ActivestudentRegistrations (DegreeID); -- 16 seconds
+-- This file will be executed with postgres -d uni -f q2Create.sql 
+-- Example:
+CREATE INDEX IDX_PHONEBOOK ON MyPhonebook(name);
