ddc834317f235e3c89d89ab807363ec1f5964210
papapetrou <odysseasp@gmail.com>
2019-02-18 09:46:38 +0100

    Update load.sh

--- load.sh
+++ load.sh
@@ -1,2 +1,8 @@
+#!/bin/sh
+# This script will create the tables and load all data. A sample template follows, but feel free to merge the two files.
+# The script will be given 20 minutes and killed afterwards
+# Expected size of tables after loading should be below 8 GB
+# The total disk space available for storing all data, views, indices, and the write-ahead-log (also see UNLOGGED keyword) is 12 GB
+
 psql -d uni -f createTables.sql
 psql -d uni -f loadData.sql
