37447914e1e918c212a32d1de7451044bba49ea7
papapetrou <odysseasp@gmail.com>
2019-02-18 12:23:54 +0100

    Create queriesAfterLoading.sql

---
+++ queriesAfterLoading.sql
@@ -0,0 +1,4 @@
+-- here you will write the three queries requested in Question 1. Each query should be in a single line, i.e., do not break long queries to more lines.
+SELECT 'Replace this line with query 1';
+SELECT 'Replace this line with query 2';
+SELECT 'Replace this line with query 3';
