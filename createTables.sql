7fe2836dce58855e31c718bc151c9c03d4c1ce06
papapetrou <odysseasp@gmail.com>
2019-02-18 09:54:06 +0100

    Update createTables.sql

--- createTables.sql
+++ createTables.sql
@@ -1,1 +1,6 @@
--- This file should create the database
+-- This file should create the tables
+-- An example follows, replace it with your own commands.
+-- SQL files can be executed from command-line using psql -d THE_DATABASE_NAME -f THE_FILENAME
+-- Notice the UNLOGGED option, which drastically increases performance and reduces space usage
+
+CREATE UNLOGGED TABLE myPhonebook(id int, name varchar(50), address varchar(200), phoneNumber varchar(100));
