d97e284555abed4d7fcb4fb018440a1555375ff0
papapetrou <odysseasp@gmail.com>
2019-02-19 10:28:15 +0100

    Update loadData.sql

--- loadData.sql
+++ loadData.sql
@@ -1,6 +1,6 @@
 -- this file should load all data in the previously-created tables
--- the data will be stored under /var/lib/postgresql/tables
--- for example, the Students file is under /var/lib/postgresql/tables/Students.table 
+-- the data will be stored under /mnt/ramdisk/tables
+-- for example, the Students file is under /mnt/ramdisk/tables/Students.table 
 -- The files of the folder are as follows (mind the lower-case/upper-case): 
 --   CourseOffers.table, CourseRegistrations.table, Courses.table, Degrees.table
 --   StudentAssistants.table, StudentRegistrationsToDegrees.table, Students.table
@@ -8,6 +8,6 @@
 -- Don't forget to analyze at the end. It can make a difference in query performance.
 -- Example:
 
-COPY myPhonebook(id, name, address,phoneNumber) FROM '/var/lib/postgresql/tables/myphonebook.table' DELIMITER ',' CSV HEADER;
+COPY myPhonebook(id, name, address,phoneNumber) FROM '/mnt/ramdisk/tables/myphonebook.table' DELIMITER ',' CSV HEADER;
 
 ANALYZE VERBOSE
